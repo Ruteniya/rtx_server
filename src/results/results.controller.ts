0ba5e67d8ee83ac3e1566132534e9bcc6d7ba533
Mariia Padalka <ms.padalka@gmail.com>
2026-02-04 00:02:01 +0200

    Enhance ResultsController by adding logging functionality for result generation, retrieval, and CSV export operations.

--- src/results/results.controller.ts
+++ src/results/results.controller.ts
@@ -3,26 +3,30 @@ import { ResultsService } from './results.service'
 
 import { AdminAuth, SystemAuth } from 'src/decorators'
 import { Dto } from 'src/dto'
+import { CustomLogger } from 'src/utils'
 
 @Controller('results')
 export class ResultsController {
-  constructor(private readonly resultsService: ResultsService) {}
+  constructor(private readonly resultsService: ResultsService, private readonly logger: CustomLogger) {}
 
   @SystemAuth()
   @Post()
   generateResults() {
+    this.logger.log('Generating results')
     return this.resultsService.generateResults()
   }
 
   @AdminAuth()
   @Get()
   findAll(@Query() query: Dto.Results.ResultsListQueryDto) {
+    this.logger.log('Finding all results', query)
     return this.resultsService.findAll(query)
   }
 
   @AdminAuth()
   @Get('export/csv')
   exportCsv(): Promise<{ url: string }> {
+    this.logger.log('Starting results CSV export')
     return this.resultsService.exportResultsCsv()
   }
 }
