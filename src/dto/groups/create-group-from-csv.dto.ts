6fa9f4393edad974210dba099d54823075d1cde9
Mariia Padalka <ms.padalka@gmail.com>
2026-02-05 17:35:22 +0200

    Enhance CsvGroup DTO by adding transformation decorators to trim strings and convert number of participants from string to number. This improves data integrity when processing CSV input.

--- src/dto/groups/create-group-from-csv.dto.ts
+++ src/dto/groups/create-group-from-csv.dto.ts
@@ -1,20 +1,24 @@
 import { IsArray, IsNumber, IsString, ArrayMinSize, ArrayMaxSize, IsEmail, ValidateNested } from 'class-validator'
-import { Type } from 'class-transformer'
+import { Transform, Type } from 'class-transformer'
 import { Pto } from 'rtxtypes'
 
 export class CsvGroup implements Pto.Groups.CsvGroup {
   @IsString()
+  @Transform(({ value }) => value.trim())
   name: string
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   categoryName: string
 
   @IsNumber()
+  @Transform(({ value }) => Number(value))
   numberOfParticipants: number
 
   @IsArray()
   @ArrayMaxSize(3)
   @IsEmail({}, { each: true })
+  @Transform(({ value }) => value.map((email: string) => email.trim()))
   emails: string[]
 }
 
