289b230bf10bbafdc5d292b1c0fa3d14019f4e2e
Mariia Padalka <ms.padalka@gmail.com>
2026-03-14 19:28:35 +0200

    Refactor database configuration in databaseConfig.ts to utilize environment variables for development and production settings. Update .sequelizerc to use __dirname for path resolution, ensuring correct file paths.

--- src/databaseConfig.ts
+++ src/databaseConfig.ts
@@ -1,3 +1,28 @@
-import { postgresSettings } from 'src/settings'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-module.exports = { ...postgresSettings }
+const development = {
+  port: parseInt(process.env.DATABASE_PORT || '3306'),
+  database: process.env.DATABASE_NAME || 'rtx',
+  username: process.env.DATABASE_USER || 'rtx',
+  password: process.env.DATABASE_PASSWORD || 'password',
+  host: process.env.DATABASE_HOST || 'localhost',
+  dialect: 'postgres' as const,
+  logging: console.log,
+  ssl: false
+}
+
+const production = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+      dialect: 'postgres' as const,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    }
+  : development
+
+module.exports = { development, production }
